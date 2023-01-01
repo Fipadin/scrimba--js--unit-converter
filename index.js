8806f6acf099eb0304192cac75e0c1eaee6ab5fe
Fer <88008613+Fipadin@users.noreply.github.com>
2023-01-01 23:51:25 +0100

    Almost finished. Just some final touches to the js file to make it perfect
    Yeah, that.

---
+++ index.js
@@ -0,0 +1,5 @@
+// 1. tomar la unidad del usuario y guardarla en una variable
+// 2. multiplicarla por los conversores
+// 3. display it debajo de los títulos con línea de texto
+
+const userInput = 
